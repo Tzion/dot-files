dd644a115b4baaaebaee06f53565fe47d8bc2435
Tzion <tzion12345@gmail.com>
2023-08-21 13:38:19 +0300

    Add files via upload
    add scripts directory and url_esacping script

---
+++ url_escaping.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Color codes
+GREEN="\033[1;32m"   # Bright green
+PINK="\033[1;95m"    # Bright pink
+RESET="\033[0m"
+
+
+unescape() {
+	if [ -n "$1" ]; then
+		url=$(echo -n "$1")
+	else
+		url=$(pbpaste)
+	fi
+	result=$(echo -n "$url" | perl -MURI::Escape -ne 'chomp;print uri_unescape($_),"\n"')
+
+	printf "${GREEN}Unescaped URL:${RESET}\n"
+	printf "\t${PINK}%s${RESET}\n" "$result"
+	echo -n "$result" | pbcopy
+	printf "${GREEN}Copied to clipboard${RESET}\n"
+}
+
+alias url_unescaping="unescape"
+
